3c9fb6be289501af74f4e9ddf8f7615099dbdf75
wan7 <rrsyycm@163.com>
2023-02-17 17:14:36 +0800

    Apis+json

--- apis/apis-biz/src/main/java/cc/wan7/bear/apis/entity/Apis.java
+++ apis/apis-biz/src/main/java/cc/wan7/bear/apis/entity/Apis.java
@@ -16,13 +16,20 @@
  */
 package cc.wan7.bear.apis.entity;
 
+
+import com.alibaba.fastjson.JSONArray;
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import cc.wan7.bear.common.mybatis.base.BaseEntity;
+import com.baomidou.mybatisplus.extension.handlers.JacksonTypeHandler;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import java.util.List;
+//import org.springframework.boot.configurationprocessor.json.JSONArray;
 
 /**
  * apis 表
@@ -31,119 +38,123 @@ import lombok.EqualsAndHashCode;
  * @date 2023-02-16 15:11:40
  */
 @Data
-@TableName("apis")
+//@TableName("apis")
+@TableName(value = "apis",autoResultMap = true)
 @EqualsAndHashCode(callSuper = true)
 @Schema(description = "apis 表")
 public class Apis extends BaseEntity {
 
-    /**
-     * 主键
-     */
-    @TableId(type = IdType.ASSIGN_ID)
-    @Schema(description ="主键")
-    private Long id;
+	/**
+	 * 主键
+	 */
+	@TableId(type = IdType.ASSIGN_ID)
+	@Schema(description = "主键")
+	private Long id;
 
-    /**
-     * 接口命名空间
-     */
-    @Schema(description ="接口命名空间")
-    private String apiNamespace;
+	/**
+	 * 接口命名空间
+	 */
+	@Schema(description = "接口命名空间")
+	private String apiNamespace;
 
-    /**
-     * 接口服务名称
-     */
-    @Schema(description ="接口服务名称")
-    private String apiName;
+	/**
+	 * 接口服务名称
+	 */
+	@Schema(description = "接口服务名称")
+	private String apiName;
 
-    /**
-     * 接口模块名称
-     */
-    @Schema(description ="接口模块名称")
-    private String apiModules;
+	/**
+	 * 接口模块名称
+	 */
+	@Schema(description = "接口模块名称")
+	private String apiModules;
 
-    /**
-     * 接口服务名称
-     */
-    @Schema(description ="接口服务名称")
-    private String apiTitle;
+	/**
+	 * 接口服务名称
+	 */
+	@Schema(description = "接口服务名称")
+	private String apiTitle;
 
-    /**
-     * 接口服务描述
-     */
-    @Schema(description ="接口服务描述")
-    private String apiDesc;
+	/**
+	 * 接口服务描述
+	 */
+	@Schema(description = "接口服务描述")
+	private String apiDesc;
 
-    /**
-     * 接口请求参数规则
-     */
-    @Schema(description ="接口请求参数规则")
-    private String apiRequestParams;
+	/**
+	 * 接口请求参数规则
+	 */
+	@Schema(description = "接口请求参数规则")
+	private String apiRequestParams;
 
-    /**
-     * 接口返回参数规则
-     */
-    @Schema(description ="接口返回参数规则")
-    private String apiResponseParams;
+	/**
+	 * 接口返回参数规则
+	 */
+	@Schema(description = "接口返回参数规则")
+	private String apiResponseParams;
 
-    /**
-     * 	接口返回示例(json)
-     */
-    @Schema(description ="	接口返回示例(json)")
-    private String apiResponseExample;
+	/**
+	 * 接口返回示例(json)
+	 */
+	@Schema(description = "	接口返回示例(json)")
+	@TableField(typeHandler = JacksonTypeHandler.class)
+	private JSONArray apiResponseExample;
 
-    /**
-     * 是否隐藏接口文档，0否1是
-     */
-    @Schema(description ="是否隐藏接口文档，0否1是")
-    private String isHideDoc;
+//    private String apiResponseExample;
 
-    /**
-     * 请求方式
-     */
-    @Schema(description ="请求方式")
-    private String requestMethod;
+	/**
+	 * 是否隐藏接口文档，0否1是
+	 */
+	@Schema(description = "是否隐藏接口文档，0否1是")
+	private String isHideDoc;
 
-    /**
-     * 	是否生成数据接口API，同时创建数据库表，1是0否
-     */
-    @Schema(description ="	是否生成数据接口API，同时创建数据库表，1是0否")
-    private String isDataApi;
+	/**
+	 * 请求方式
+	 */
+	@Schema(description = "请求方式")
+	private String requestMethod;
 
-    /**
-     * 完整的接口代码
-     */
-    @Schema(description ="完整的接口代码")
-    private String apiCode;
+	/**
+	 * 是否生成数据接口API，同时创建数据库表，1是0否
+	 */
+	@Schema(description = "	是否生成数据接口API，同时创建数据库表，1是0否")
+	private String isDataApi;
 
-    /**
-     * 	简易的接口函数代码
-     */
-    @Schema(description ="	简易的接口函数代码")
-    private String apiFunctionCode;
+	/**
+	 * 完整的接口代码
+	 */
+	@Schema(description = "完整的接口代码")
+	private String apiCode;
 
-    /**
-     * API开发模式，0完整模式1简易模式
-     */
-    @Schema(description ="API开发模式，0完整模式1简易模式")
-    private String apiDevMode;
+	/**
+	 * 简易的接口函数代码
+	 */
+	@Schema(description = "	简易的接口函数代码")
+	private String apiFunctionCode;
 
-    /**
-     * 数据库配置id
-     */
-    @Schema(description ="数据库配置id")
-    private Long databaseId;
+	/**
+	 * API开发模式，0完整模式1简易模式
+	 */
+	@Schema(description = "API开发模式，0完整模式1简易模式")
+	private String apiDevMode;
 
-    /**
-     * 数据库对应的表名字
-     */
-    @Schema(description ="数据库对应的表名字")
-    private String databaseTableName;
+	/**
+	 * 数据库配置id
+	 */
+	@Schema(description = "数据库配置id")
+	private Long databaseId;
 
-    /**
-     * 是否使用CURD基类接口
-     */
-    @Schema(description ="是否使用CURD基类接口")
-    private String isCurd;
+	/**
+	 * 数据库对应的表名字
+	 */
+	@Schema(description = "数据库对应的表名字")
+	private String databaseTableName;
+
+	/**
+	 * 是否使用CURD基类接口
+	 */
+	@Schema(description = "是否使用CURD基类接口")
+	private String isCurd;
 
 
 }
---
+++ apis/apis-biz/src/main/java/cc/wan7/bear/apis/entity/ResponseExample.java
@@ -0,0 +1,13 @@
+package cc.wan7.bear.apis.entity;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import lombok.Data;
+
+@Data
+public class ResponseExample {
+//	@JSONField(name = "text1")
+	private String text1;
+
+//	@JSONField(name = "text2")
+	private String text2;
+}
